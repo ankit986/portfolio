7ba23d78f8ec60ff3c289db410d84340ce936d2e
ankit986 <ab98647@gmail.com>
2019-11-01 15:53:57 +0530

    Updates

---
+++ precache-manifest.61c28530bf44d603e3a432b74caf047e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a809f990906f62cf6b734b8671c050",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "365372f3e588175f562a",
+    "url": "/portfolio/static/css/2.4c2a2e3e.chunk.css"
+  },
+  {
+    "revision": "169a5d5f3a398ac4935e",
+    "url": "/portfolio/static/css/main.4c7bdc31.chunk.css"
+  },
+  {
+    "revision": "365372f3e588175f562a",
+    "url": "/portfolio/static/js/2.97e8d256.chunk.js"
+  },
+  {
+    "revision": "169a5d5f3a398ac4935e",
+    "url": "/portfolio/static/js/main.0242836e.chunk.js"
+  },
+  {
+    "revision": "c5306228cc83f6f6271c",
+    "url": "/portfolio/static/js/runtime-main.7676bd13.js"
+  },
+  {
+    "revision": "6dfb9a40a1a974965a63f379c61934b0",
+    "url": "/portfolio/static/media/AnkitBansal_CV.6dfb9a40.pdf"
+  },
+  {
+    "revision": "1bb10abf90a96d986d39d65259c0b1d2",
+    "url": "/portfolio/static/media/profileImage.1bb10abf.png"
+  }
+]);
