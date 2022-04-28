693fde884844c023f104af3902b963d9e6946a05
ankit986 <ab98647@gmail.com>
2022-04-28 22:14:13 +0530

    Updates

---
+++ precache-manifest.dc84eb2443a29cc389c8401ddeff0034.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db5fa8c4b7b7f02e5535c06d5b3ffc7",
+    "url": "/ayushi/index.html"
+  },
+  {
+    "revision": "e829c355bbe109362ebb",
+    "url": "/ayushi/static/css/2.764e46c2.chunk.css"
+  },
+  {
+    "revision": "f9b3d2a5ea9a5944814c",
+    "url": "/ayushi/static/css/main.37a0195c.chunk.css"
+  },
+  {
+    "revision": "e829c355bbe109362ebb",
+    "url": "/ayushi/static/js/2.efe49511.chunk.js"
+  },
+  {
+    "revision": "f9b3d2a5ea9a5944814c",
+    "url": "/ayushi/static/js/main.acd04b94.chunk.js"
+  },
+  {
+    "revision": "37dce2dc0e9b5e693277",
+    "url": "/ayushi/static/js/runtime-main.4b27d4e7.js"
+  },
+  {
+    "revision": "de66188620b207f0a9bdede2c94b0e71",
+    "url": "/ayushi/static/media/AnkitBansal_CV.de661886.pdf"
+  },
+  {
+    "revision": "d10778e877f8a40fe4720075ade3e19f",
+    "url": "/ayushi/static/media/caring.d10778e8.jpeg"
+  },
+  {
+    "revision": "e239d984668fc9fe5dd6cff5dd2d0179",
+    "url": "/ayushi/static/media/cute.e239d984.jpeg"
+  },
+  {
+    "revision": "e0c026f88c65fb7a0f47801de6fa3030",
+    "url": "/ayushi/static/media/dreamer.e0c026f8.jpeg"
+  },
+  {
+    "revision": "32a6c886ae4d7486a616da742a9f4339",
+    "url": "/ayushi/static/media/hbd.32a6c886.jpg"
+  },
+  {
+    "revision": "6f74c43c6b61bd30fc26cec1fcd8624b",
+    "url": "/ayushi/static/media/hbdd12.6f74c43c.png"
+  },
+  {
+    "revision": "b8e4eb54827df4ad7ac19075e6f75207",
+    "url": "/ayushi/static/media/lame.b8e4eb54.jpeg"
+  },
+  {
+    "revision": "54bcc37580a40b72369b4f401c730d7d",
+    "url": "/ayushi/static/media/ojos.54bcc375.jpeg"
+  },
+  {
+    "revision": "796541527b9865a2ef2b3fa85dabac3b",
+    "url": "/ayushi/static/media/posessive.79654152.jpeg"
+  },
+  {
+    "revision": "633b8927d520ca121c6ad9eea6dc1e40",
+    "url": "/ayushi/static/media/pretty.633b8927.jpeg"
+  },
+  {
+    "revision": "90c16ae0006f111a145aa5f4781f25ea",
+    "url": "/ayushi/static/media/profileImage.90c16ae0.webp"
+  },
+  {
+    "revision": "e83436150d718734df9d84cb5198193b",
+    "url": "/ayushi/static/media/supportive.e8343615.jpeg"
+  },
+  {
+    "revision": "dc7e97f6709e23eddb2ba51e31a49186",
+    "url": "/ayushi/static/media/unpredictable.dc7e97f6.jpeg"
+  },
+  {
+    "revision": "10afc7545e3277196afb9ea934849a30",
+    "url": "/ayushi/static/media/writter.10afc754.jpeg"
+  }
+]);
